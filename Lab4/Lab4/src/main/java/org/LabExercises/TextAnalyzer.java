14b7ed04a77c0d802ff1748f7c3923ae1b9836ed
sideshowbobgot <sideshowbobgot@gmail.com>
2023-06-11 15:19:16 +0300

    lab4: in progress

--- Lab4/Lab4/src/main/java/org/LabExercises/TextAnalyzer.java
+++ Lab4/Lab4/src/main/java/org/LabExercises/TextAnalyzer.java
@@ -1,9 +1,17 @@
 package org.LabExercises;
 
+import opennlp.tools.cmdline.lemmatizer.LemmatizerMETool;
+import opennlp.tools.lemmatizer.Lemmatizer;
+import opennlp.tools.lemmatizer.LemmatizerME;
+import opennlp.tools.lemmatizer.LemmatizerModel;
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSTaggerME;
 import opennlp.tools.sentdetect.SentenceDetectorME;
 import opennlp.tools.sentdetect.SentenceModel;
 import opennlp.tools.tokenize.TokenizerME;
 import opennlp.tools.tokenize.TokenizerModel;
+import opennlp.tools.util.ObjectStream;
+import org.springframework.util.StreamUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -12,10 +20,13 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.concurrent.ForkJoinPool;
+import java.util.stream.Stream;
 
 public class TextAnalyzer {
-	private static final String SENTENCE_MODEL_PATH = "opennlp-en-ud-ewt-sentence-1.0-1.9.3.bin";
-	private static final String TOKEN_MODEL_PATH = "opennlp-en-ud-ewt-tokens-1.0-1.9.3.bin";
+	private static final String SENTENCE_MODEL_PATH = "en-sent.bin";
+	private static final String TOKEN_MODEL_PATH = "en-token.bin";
+	private static final String LEMMATIZER_MODEL_PATH = "en-lemmatizer.bin";
+	private static final String POSTAGGER_MODEL_PATH = "en-pos-maxent.bin";
 	private static final String NOT_VALID_SYMBOLS = "[^a-zA-Z\s\n]";
 	private static final String WHITE_SPACE = " ";
 	private static final String LINE_FEED = "\n";
@@ -23,6 +34,9 @@ public class TextAnalyzer {
 	private final ForkJoinPool pool = ForkJoinPool.commonPool();
 	private SentenceDetectorME sentenceDetector;
 	private TokenizerME tokenizer;
+	private LemmatizerME lemmatizer;
+	private POSTaggerME posTagger;
+
 	private static TextAnalyzer fileStringReader;
 
 	public static TextAnalyzer getInstance() {
@@ -40,26 +54,44 @@ public class TextAnalyzer {
 			var tokModelIn = new FileInputStream(TOKEN_MODEL_PATH);
 			var tokModel = new TokenizerModel(tokModelIn);
 			tokenizer = new TokenizerME(tokModel);
+			var lemModelIn = new FileInputStream(LEMMATIZER_MODEL_PATH);
+			var lemModel = new LemmatizerModel(lemModelIn);
+			lemmatizer = new LemmatizerME(lemModel);
+			var posModelIn = new FileInputStream(POSTAGGER_MODEL_PATH);
+			var posModel = new POSModel(posModelIn);
+			posTagger = new POSTaggerME(posModel);
 		} catch(IOException e) {
-			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
 	}
 
-	public double getAverageLength(String filePath) throws FileNotFoundException {
-		var text = readFile(filePath);
-		var average = Arrays.stream(sentenceDetector.sentDetect(text))
-			.map(s -> s.replaceAll(NOT_VALID_SYMBOLS, EMPTY_SPACE))
-			.map(String::toLowerCase).map(s -> tokenizer.tokenize(s))
-			.flatMap(Arrays::stream).mapToDouble(String::length).average();
+	public String[] getCommonWords(String fileNameOne, String fileNameTwo) {
+		var tokens = Stream.of(fileNameOne, fileNameTwo)
+			.map(s -> getTokenStream(s).toArray(String[]::new))
+			.toArray(String[][]::new);
+		var tags = Arrays.stream(tokens).map(posTagger::tag)
+			.toArray(String[][]::new);
+	}
+
+	public Stream<String[]> getTokenStream(String fileName) {
+		try {
+			String text = readFile(fileName);
+			return Arrays.stream(sentenceDetector.sentDetect(text))
+				.map(s -> s.replaceAll(NOT_VALID_SYMBOLS, EMPTY_SPACE))
+				.map(s -> tokenizer.tokenize(s));
+		} catch(FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public double getAverageLength(String filePath) {
+		var average = getTokenStream(filePath).flatMap(Arrays::stream)
+			.mapToDouble(String::length).average();
 		return average.isPresent() ? average.getAsDouble() : 0;
 	}
 
-	public double getAverageLengthForkJoin(String filePath) throws IOException {
-		var text = readFile(filePath);
-		var tokens = Arrays.stream(sentenceDetector.sentDetect(text))
-			.map(s -> s.replaceAll(NOT_VALID_SYMBOLS, EMPTY_SPACE))
-			.map(String::toLowerCase).map(s -> tokenizer.tokenize(s))
-			.toArray(String[][]::new);
+	public double getAverageLengthForkJoin(String filePath) {
+		var tokens = getTokenStream(filePath).toArray(String[][]::new);
 		var res = pool.invoke(new AverageLengthForkJoin(0, tokens));
 		return (double) res.getFirst() / res.getSecond();
 	}
