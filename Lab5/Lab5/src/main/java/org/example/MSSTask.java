c414391886a0152f90b030de21413874655b5a7f
sideshowbobgot <sideshowbobgot@gmail.com>
2023-06-16 17:43:17 +0300

    lab5: in progress

--- Lab5/Lab5/src/main/java/org/example/MSSTask.java
+++ Lab5/Lab5/src/main/java/org/example/MSSTask.java
@@ -1,10 +1,23 @@
 package org.example;
 
 public class MSSTask implements Runnable {
+	private static final long SLEEP_TIME = 1000;
+	public MSSTaskStatus status = MSSTaskStatus.Waiting;
+	public long startTimeQueue = -1;
+	public long endTimeQueue = -1;
 
+	public MSSTask() {}
 
 	@Override
 	public void run() {
-
+		status = MSSTaskStatus.Running;
+		startTimeQueue = System.currentTimeMillis();
+		try {
+			Thread.sleep(SLEEP_TIME);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		endTimeQueue = System.currentTimeMillis();
+		status = MSSTaskStatus.Finished;
 	}
 }
---
+++ Lab5/Lab5/src/main/java/org/example/MSSTaskStatus.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public enum MSSTaskStatus {
+	Cancelled,
+	Waiting,
+	Running,
+	Finished
+}
