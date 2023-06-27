3a015a07aecc8377d417058df600712305b0c0ff
sideshowbobgot <sideshowbobgot@gmail.com>
2023-06-27 22:39:08 +0300

    CourseWork: in progress

--- CourseWork/TAStar.h
+++ CourseWork/TAStar.h
@@ -13,7 +13,7 @@
 namespace AStar {
 	
 	template<typename T>
-	concept IsCostType = std::three_way_comparable<T> && requires(T t) { t + t; };
+	concept IsCostType = std::is_default_constructible_v<T> && std::three_way_comparable<T> && requires(T t) { t + t; };
 	
 	template<typename DataType, typename CostType> requires IsCostType<CostType>
 	class Node;
@@ -26,6 +26,7 @@ namespace AStar {
 		
 		public:
 		void AddNeighbours(const std::map<std::shared_ptr<Node>, CostType>& neighbours);
+		const std::map<std::shared_ptr<Node>, CostType>& Neighbours() const;
 		const std::shared_ptr<DataType>& Data() const;
 		
 		protected:
@@ -42,6 +43,13 @@ namespace AStar {
 	
 	template<typename DataType, typename CostType>
 	requires IsCostType<CostType>
+	const std::map<std::shared_ptr<Node<DataType, CostType>>, CostType>&
+	Node<DataType, CostType>::Neighbours() const {
+		return m_mNeighbours;
+	}
+	
+	template<typename DataType, typename CostType>
+	requires IsCostType<CostType>
 	const std::shared_ptr<DataType>& Node<DataType, CostType>::Data() const {
 		return m_pData;
 	}
@@ -54,11 +62,13 @@ namespace AStar {
 		
 		public:
 		using PNode = std::shared_ptr<Node<DataType, CostType>>;
+		using PNodes = std::vector<PNode>;
 		using Heuristic = std::function<CostType(const PNode&, const PNode&)>;
 		
 		virtual std::vector<PNode> Solve(const PNode& start, const PNode& end,
-										 const std::vector<PNode>& nodes, const Heuristic& heuristic);
+										 const PNodes& nodes, const Heuristic& heuristic);
 		
+		protected:
 		
 	};
 	
@@ -67,8 +77,22 @@ namespace AStar {
 	std::vector<typename TAStar<DataType, CostType>::PNode>
 	TAStar<DataType, CostType>::Solve(const PNode& start,
 									  const PNode& end,
-									  const std::vector<PNode>& nodes,
+									  const PNodes& nodes,
 									  const Heuristic& heuristic) {
+		
+		auto paths = std::vector<PNodes>();
+		paths.push_back({start});
+		if(start == end) {
+			return paths.front();
+		}
+		
+		auto isStop = false;
+		while(!isStop) {
+			for(auto& path:paths) {
+				const auto& lastNode = path.back();
+				
+			}
+		}
 		
 	}
 }
