04a884858637e6f325b35e13e98f452f525081fe
sideshowbobgot <sideshowbobgot@gmail.com>
2023-06-27 20:37:39 +0300

    CourseWork: in progress

--- CourseWork/AStar.cpp
+++ CourseWork/AStar.cpp
@@ -3,27 +3,3 @@
 //
 
 #include "TAStar.h"
-#include <ranges>
-
-TAStar::Nodes TAStar::Solve(const int start, const int end, const Nodes& nodes,
-							const Vertexes& vertexes) {
-	
-	auto paths = std::vector<Nodes>();
-	paths.push_back({start});
-	
-	if(start == end) {
-		return paths.front();
-	}
-	
-	auto inspectedNodes = Nodes();
-	
-	auto isStop = false;
-	while(!isStop) {
-		for(auto& path:paths) {
-			const auto& neighbours = vertexes[path.back()];
-			auto vals = std::views::transform(neighbours, []())
-		}
-	}
-	
-	
-}
--- CourseWork/TAStar.h
+++ CourseWork/TAStar.h
@@ -10,52 +10,71 @@
 #include <functional>
 #include <memory>
 
-template<typename T>
-concept IsCostType = std::three_way_comparable<T> && requires(T t) { t + t; };
-
-template<typename DataType, typename CostType> requires IsCostType<CostType>
-class Node {
-	public:
-	Node() = default;
-	virtual ~Node() = default;
+namespace AStar {
 	
-	public:
-	void AddNeighbours(const std::map<std::shared_ptr<Node>, CostType>& neighbours);
-	const std::shared_ptr<DataType>& Data() const;
+	template<typename T>
+	concept IsCostType = std::three_way_comparable<T> && requires(T t) { t + t; };
 	
-	protected:
-	std::shared_ptr<DataType> m_pData = nullptr;
-	std::map<std::shared_ptr<Node>, CostType> m_mNeighbours;
-};
-
-template<typename DataType, typename CostType>
-requires IsCostType<CostType>
-void Node<DataType, CostType>::AddNeighbours(
-	const std::map<std::shared_ptr<Node<DataType, CostType>>, CostType>& neighbours) {
-	std::copy(neighbours.begin(), neighbours.end(), std::back_inserter(m_mNeighbours));
+	template<typename DataType, typename CostType> requires IsCostType<CostType>
+	class Node;
+	
+	template<typename DataType, typename CostType> requires IsCostType<CostType>
+	using PNode = std::shared_ptr<Node<DataType, CostType>>;
+	
+	template<typename DataType, typename CostType> requires IsCostType<CostType>
+	class Node {
+		public:
+		Node() = default;
+		virtual ~Node() = default;
+		
+		public:
+		void AddNeighbours(const std::map<PNode<DataType, CostType>, CostType>& neighbours);
+		const std::shared_ptr<DataType>& Data() const;
+		
+		protected:
+		std::shared_ptr<DataType> m_pData = nullptr;
+		std::map<PNode<DataType, CostType>, CostType> m_mNeighbours;
+	};
+	
+	template<typename DataType, typename CostType>
+	requires IsCostType<CostType>
+	void Node<DataType, CostType>::AddNeighbours(
+		const std::map<PNode<DataType, CostType>, CostType>& neighbours) {
+		std::copy(neighbours.begin(), neighbours.end(), std::back_inserter(m_mNeighbours));
+	}
+	
+	template<typename DataType, typename CostType>
+	requires IsCostType<CostType>
+	const std::shared_ptr<DataType>& Node<DataType, CostType>::Data() const {
+		return m_pData;
+	}
+	
+	template<typename DataType, typename CostType> requires IsCostType<CostType>
+	class TAStar {
+		public:
+		TAStar() = default;
+		virtual~ TAStar() = default;
+		
+		public:
+		using Heuristic = std::function<CostType(const std::shared_ptr<Node<DataType, CostType>>&,
+												 const std::shared_ptr<Node<DataType, CostType>>&)>;
+		
+		virtual std::vector<PNode<DataType, CostType>> Solve(const PNode<DataType, CostType>& start,
+															 const PNode<DataType, CostType>& end,
+															 const std::vector<PNode<DataType, CostType>>& nodes,
+															 const Heuristic& heuristic) const;
+	};
+	
+	template<typename DataType, typename CostType>
+	requires IsCostType<CostType>
+	std::vector<PNode<DataType, CostType>> TAStar<DataType, CostType>::Solve(const PNode<DataType, CostType>& start,
+																			 const PNode<DataType, CostType>& end,
+																			 const std::vector<PNode<DataType, CostType>>& nodes,
+																			 const Heuristic& heuristic) const {
+		
+		
+	}
 }
-
-template<typename DataType, typename CostType>
-requires IsCostType<CostType>
-const std::shared_ptr<DataType>& Node<DataType, CostType>::Data() const {
-	return m_pData;
-}
-
-template<typename DataType, typename CostType> requires IsCostType<CostType>
-class TAStar {
-	public:
-	TAStar() = default;
-	virtual~ TAStar() = default;
-	
-	public:
-	using Heuristic = std::function<CostType(const std::shared_ptr<Node<DataType, CostType>>&,
-											 const std::shared_ptr<Node<DataType, CostType>>&)>;
-	using Nodes = std::vector<int>;
-	using Vertexes = std::vector<std::vector<std::pair<int, double>>>;
-	virtual Nodes Solve(const int start, const int end,
-						const Nodes& nodes, const Vertexes& vertexes);
-	
-};
 
 
 #endif //COURSEWORK_TASTAR_H
