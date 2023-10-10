ea2ca72d378108738ab5265a049e83746404edf5
sideshowbobgot <sideshowbobgot@gmail.com>
2023-10-10 22:27:32 +0300

    CourseWork: in progress

--- CourseWorkRust/src/my_bfs.rs
+++ CourseWorkRust/src/my_bfs.rs
@@ -39,15 +39,19 @@ pub fn find_path<T>(from: SingleNode<T>, to: SingleNode<T>, graph: Graph<T>, tot
     None
 }
 
-enum ChoosePathResult<T> {
-    Locked,
+#[derive(Debug)]
+enum BFSError {
+    Locked
+}
+
+enum ChoosePathMarker<T> {
     DeadEnd(SinglePath<T>),
     PathChosen((SinglePath<T>, Vec<SingleNode<T>>)),
     NoPaths,
     FinalPathFound(SinglePath<T>),
 }
 
-enum UpdatePathsResult<T> {
+enum UpdatePathsMarker<T> {
     DeadEnd(SinglePath<T>),
     Updated
 }
@@ -71,23 +75,24 @@ impl<T: Hash> ThreadTask<T> {
     fn execute(&mut self) {
         let mut rand_gen = ThreadRng::default();
         while *self.comm_mark.read().unwrap() {
-            match self.choose_path(&mut rand_gen) {
-                ChoosePathResult::Locked => {}
-                ChoosePathResult::DeadEnd(path) => self.remove_path(path),
-                ChoosePathResult::PathChosen((path, unvisited_nodes)) => {
+            let marker = self.choose_path(&mut rand_gen);
+            if marker.is_err() {continue;}
+            match marker.unwrap() {
+                ChoosePathMarker::DeadEnd(path) => self.remove_path(path),
+                ChoosePathMarker::PathChosen((path, unvisited_nodes)) => {
                     match self.update_paths(path, unvisited_nodes) {
-                        UpdatePathsResult::DeadEnd(chosen_path) => {
+                        UpdatePathsMarker::DeadEnd(chosen_path) => {
                             self.remove_path(chosen_path);
                         }
-                        UpdatePathsResult::Updated => {}
+                        UpdatePathsMarker::Updated => {}
                     }
                 },
-                ChoosePathResult::NoPaths => {
+                ChoosePathMarker::NoPaths => {
                     let mut mark_write = self.comm_mark.write().unwrap();
                     *mark_write = false;
                     break;
                 },
-                ChoosePathResult::FinalPathFound(path) => {
+                ChoosePathMarker::FinalPathFound(path) => {
                     let mut mark_write = self.comm_mark.write().unwrap();
                     if *mark_write {
                         *mark_write = false;
@@ -101,14 +106,11 @@ impl<T: Hash> ThreadTask<T> {
     }
 
     fn update_paths(&mut self, chosen_path: SinglePath<T>, unvisited_nodes: Vec<SingleNode<T>>)
-                    -> UpdatePathsResult<T> {
-
+                    -> UpdatePathsMarker<T> {
         let mut chosen_path_write = chosen_path.write().unwrap();
         let last_node = chosen_path_write.last().unwrap();
-
         let mut visited_map_write = self.visited_map.write().unwrap();
         let visited_nodes_opt = &visited_map_write.get(last_node);
-
         let sure_unvisited = if visited_nodes_opt.is_none() {
             unvisited_nodes
         } else {
@@ -117,23 +119,17 @@ impl<T: Hash> ThreadTask<T> {
                 return !visited_nodes.contains(neighbour_node);
             }).map(|n| n.clone()).collect::<Vec<_>>()
         };
-
         if sure_unvisited.is_empty() {
-            return UpdatePathsResult::DeadEnd(chosen_path.clone());
+            return UpdatePathsMarker::DeadEnd(chosen_path.clone());
         }
-
         if visited_nodes_opt.is_none() {
             visited_map_write.insert(last_node.clone(), vec![]);
         }
-
         let visited_nodes = visited_map_write.get_mut(last_node).unwrap();
-
         let mut paths_write = self.paths.write().unwrap();
-
         let last_index = sure_unvisited.len() - 1;
         for (i, node) in sure_unvisited.into_iter().enumerate() {
             visited_nodes.push(node.clone());
-
             if i == last_index {
                 chosen_path_write.push_back(node);
             } else {
@@ -142,53 +138,31 @@ impl<T: Hash> ThreadTask<T> {
                 paths_write.push(Arc::new(RwLock::new(new_path)));
             }
         }
-
-        UpdatePathsResult::Updated
+        UpdatePathsMarker::Updated
     }
 
-    fn choose_path(&mut self, rand_gen: &mut ThreadRng) -> ChoosePathResult<T> {
-        let paths_read = {
-            let paths_try_read = self.paths.try_read();
-            if paths_try_read.is_err() { return ChoosePathResult::Locked; }
-            paths_try_read.unwrap()
-        };
-
-        if paths_read.is_empty() { return ChoosePathResult::NoPaths; }
-
-        let visited_map_read = {
-            let visited_map_try_read = self.visited_map.try_read();
-            if visited_map_try_read.is_err() { return ChoosePathResult::Locked; }
-            visited_map_try_read.unwrap()
-        };
-
+    fn choose_path(&mut self, rand_gen: &mut ThreadRng) -> Result<ChoosePathMarker<T>, BFSError> {
+        let paths_read = self.paths.try_read().map_err(|_| BFSError::Locked)?;
+        if paths_read.is_empty() { return Ok(ChoosePathMarker::NoPaths); }
+        let visited_map_read = self.visited_map.try_read().map_err(|_| BFSError::Locked)?;
         let chosen_path = paths_read.iter().choose(rand_gen).unwrap();
-        let chosen_path_read = {
-            let chosen_path_try_read = chosen_path.try_read();
-            if chosen_path_try_read.is_err() { return ChoosePathResult::Locked; }
-            chosen_path_try_read.unwrap()
-        };
-
+        let chosen_path_read = chosen_path.try_read().map_err(|_| BFSError::Locked)?;
         let path_last_node = chosen_path_read.last().unwrap();
         if *path_last_node == self.end_node {
-            return ChoosePathResult::FinalPathFound(chosen_path.clone());
+            return Ok(ChoosePathMarker::FinalPathFound(chosen_path.clone()));
         }
-
         let last_node_neighbours = &self.graph[path_last_node];
-        let visited_nodes_opt = visited_map_read.get(path_last_node);
-
-        let unvisited_nodes = if visited_nodes_opt.is_none() {
-            last_node_neighbours.clone()
-        } else {
-            let visited_nodes = visited_nodes_opt.unwrap();
+        let unvisited_nodes = if let Some(visited_nodes) = visited_map_read.get(path_last_node) {
             last_node_neighbours.iter().filter(|neighbour_node| {
                 return !visited_nodes.contains(neighbour_node);
             }).map(|n| n.clone()).collect::<Vec<_>>()
-        };
-
-        if unvisited_nodes.is_empty() {
-            ChoosePathResult::DeadEnd(chosen_path.clone())
         } else {
-            ChoosePathResult::PathChosen((chosen_path.clone(), unvisited_nodes))
+            last_node_neighbours.clone()
+        };
+        if unvisited_nodes.is_empty() {
+            Ok(ChoosePathMarker::DeadEnd(chosen_path.clone()))
+        } else {
+            Ok(ChoosePathMarker::PathChosen((chosen_path.clone(), unvisited_nodes)))
         }
     }
 
