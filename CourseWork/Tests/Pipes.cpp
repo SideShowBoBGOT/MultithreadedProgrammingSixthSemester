b61625b58b90014abad22cd9c6d45cd05374a7db
sideshowbobgot <sideshowbobgot@gmail.com>
2024-01-07 19:42:20 +0200

    Added example for pipes

--- CourseWork/Tests/Pipes.cpp
+++ CourseWork/Tests/Pipes.cpp
@@ -3,8 +3,15 @@
 #include <ParallelBFS/TPipes.hpp>
 
 int main() {
-	const auto [w, r] = bfs::pipes::TPipeChannel<int>::New();
-	w.Write(4);
-	auto value = r.Read();
-	std::cout << value << std::endl;
+	auto [w, r] = bfs::pipes::TPipeChannel<int>::New();
+
+	auto sender = std::jthread([ww=std::move(w)]() {
+		using namespace std::chrono_literals;
+		std::this_thread::sleep_for(4s);
+		ww.Write(10);
+	});
+
+	auto listener = std::jthread([rr=std::move(r)]() {
+		std::cout << rr.Read() << std::endl;
+	});
 }
