1f47b681f4d72dfa9e41fefdb2063249522cc97b
sideshowbobgot <sideshowbobgot@gmail.com>
2024-02-23 14:40:38 +0200

    added lab6

--- lab_6/include/lab_6/lab_6/alg_type.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef ALG_TYPE_HPP
-#define ALG_TYPE_HPP
-
-enum class AlgorithmType {
-	Blocking,
-	NonBlocking
-};
-
-#endif //ALG_TYPE_HPP
--- lab_6/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <lab_6/cli_app.hpp>
-
-int main(int argc, char *argv[]) {
-	auto cli = CliApp();
-	CLI11_PARSE(cli, argc, argv);
-	return 0;
-}
