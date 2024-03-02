906bd541a666c62f00c56271cb07f518ca321f9b
sideshowbobgot <sideshowbobgot@gmail.com>
2024-03-02 20:02:49 +0200

    added benchmark

--- Lab6/scripts/gen_data.py
+++ Lab6/scripts/gen_data.py
@@ -1,9 +1,9 @@
 import os
 
 processes = [3, 4, 5, 6]
-sizes = [i for i in range(250, 1000, 250)]
+sizes = [i for i in range(500, 2750, 250)]
 types_block = ['blocking', 'nonblocking']
-for _ in range(3):
+for _ in range(5):
     for t in processes:
         for s in sizes:
             for tb in types_block:
