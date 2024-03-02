59971f420a331c3804b3f4d19e949a896404e99a
sideshowbobgot <sideshowbobgot@gmail.com>
2024-03-02 14:59:48 +0200

    separated cli_app.cpp into different files

--- Lab6/include/lab_6/constants.hpp
+++ Lab6/include/lab_6/constants.hpp
@@ -4,4 +4,9 @@
 constexpr auto FROM_MAIN_THREAD_TAG = 1;
 constexpr auto FROM_TASK_THREAD_TAG = 2;
 
+// SYSTEM DEPENDANT
+static constexpr auto MINIMUM_ALLOC_SIZE = 224 * 2;
+
+static const std::string MAIN_SHARED_MEMORY_NAME = "eeeeeeeeeeeeeiomopnohjubuybnijsdbgiuarsbifbaeuigbeslgnuiesgnieangloersiugn";
+
 #endif //LAB6_CONSTANTS_H
